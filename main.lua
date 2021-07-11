ec64dd8703d02adc3c57aa03830a7929ce4489de
drakmaniso <moussault.laurent@gmail.com>
2021-07-11 10:34:00 +0200

    Improve display system

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local utils = require "utils"
 
--- CONSTANTS --
+-- CONSTANTS ------------------------------------------------------------------
 
 local MARGIN = renoise.ViewBuilder.DEFAULT_CONTROL_MARGIN
 local SPACING = renoise.ViewBuilder.DEFAULT_CONTROL_SPACING
@@ -8,48 +8,61 @@ local CONTROL_HEIGHT = renoise.ViewBuilder.DEFAULT_CONTROL_HEIGHT
 local MINI_CONTROL_HEIGHT = renoise.ViewBuilder.DEFAULT_CONTROL_HEIGHT
 local DIALOG_BUTTON_HEIGHT = renoise.ViewBuilder.DEFAULT_DIALOG_BUTTON_HEIGHT
 
--- GLOBALS --
+-- GLOBALS --------------------------------------------------------------------
 
 local window
 local vb, views
 
--- DISPLAY --
+local main_display
 
-local function create_display(name, width, height, step)
-    local row = vb:row {style = "invisible", spacing = 0, margin = 0, height = height}
-    row:add_child(vb:space {width = 1, height = height + 2})
+-- DISPLAY --------------------------------------------------------------------
+
+local function new_display(width, height, step)
+    local display = {
+        width = width,
+        height = height,
+        step = step,
+        tops = {},
+        bottoms = {},
+        view = vb:row {style = "invisible", spacing = 0, margin = 0, height = height}
+    }
+    display.view:add_child(vb:space {width = 1, height = height + 2})
     for i = 1, width do
         local x = (i - 1) / (width - 1)
         local y = x * height
         local bottom = math.floor(y + 0.5)
         local top = height - y
-        row:add_child(
+        display.tops[i] = vb:space {width = step, height = top + 1}
+        display.bottoms[i] = vb:row {style = "plain", width = step, height = bottom + 1}
+        display.view:add_child(
             vb:column {
-                vb:space {id = name .. "_top_" .. i, width = step, height = top + 1},
-                vb:row {style = "plain", id = name .. "_bottom_" .. i, width = step, height = bottom + 1}
+                display.tops[i],
+                display.bottoms[i]
             }
         )
     end
-    row:add_child(vb:space {width = 1, height = height + 2})
-    return row
+    display.view:add_child(vb:space {width = 1, height = height + 2})
+    return display
 end
 
-local function update_display(name, width, height, fn)
-    for i = 1, width do
-        local x = (i - 1) / (width - 1)
-        local y = fn(x) * height
+local function update_display(display, fn)
+    for i = 1, display.width do
+        local x = (i - 1) / (display.width - 1)
+        local y = fn(x) * display.height --TODO: clamp fn(x)
         local bottom = math.floor(y + 0.5)
-        local top = height - bottom
-        views[name .. "_top_" .. i].height = top + 1
-        views[name .. "_bottom_" .. i].height = bottom + 1
+        local top = display.height - bottom
+        display.tops[i].height = top + 1
+        display.bottoms[i].height = bottom + 1
     end
 end
 
--- MAIN WINDOW --
+-- MAIN WINDOW ----------------------------------------------------------------
 
 local function create_main_window()
     vb = renoise.ViewBuilder()
     views = vb.views
+
+    main_display = new_display(768, 200, 1)
 
     return vb:column {
         style = "body",
@@ -66,9 +79,7 @@ local function create_main_window()
                 max = 1,
                 notifier = function()
                     update_display(
-                        "foo",
-                        256,
-                        200,
+                        main_display,
                         function(x)
                             return (1 - x) * views.foo_rotary.value
                         end
@@ -79,7 +90,7 @@ local function create_main_window()
         vb:row {
             style = "group",
             margin = MARGIN,
-            create_display("foo", 256, 200, 3)
+            main_display.view
         }
     }
 end
@@ -92,7 +103,7 @@ local function show_main_window()
     end
 end
 
--- RENOISE HOOKS --
+-- RENOISE HOOKS --------------------------------------------------------------
 
 _AUTO_RELOAD_DEBUG = true
 
